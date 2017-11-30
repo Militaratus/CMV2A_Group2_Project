1e77e6994a46531bb37aa541aad3817c614b84e3
Militaratus <jarednealon@outlook.com>
2017-11-30 13:41:47 +0100

    First Project Cat

---
+++ Assets/Scripts/BaseTarget.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseTarget : MonoBehaviour
+{
+    // Basics
+    string[] targetTraits;
+    public bool revealed = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    void PopulateTraits()
+    {
+        targetTraits = new string[4];
+
+        int randomTrait = 0;
+        for (int i = 0; i < targetTraits.Length; i++)
+        {
+            randomTrait = Random.Range(0, 10);
+
+            switch(randomTrait)
+            {
+                case 0:
+                    targetTraits[i] = "Drunk"; break;
+                case 1:
+                    targetTraits[i] = "Drunk"; break;
+                case 2:
+                    targetTraits[i] = "Drunk"; break;
+                case 3:
+                    targetTraits[i] = "Drunk"; break;
+                case 4:
+                    targetTraits[i] = "Drunk"; break;
+                case 5:
+                    targetTraits[i] = "Drunk"; break;
+                case 6:
+                    targetTraits[i] = "Drunk"; break;
+                case 7:
+                    targetTraits[i] = "Drunk"; break;
+                case 8:
+                    targetTraits[i] = "Drunk"; break;
+                case 9:
+                    targetTraits[i] = "Drunk"; break;
+                default:
+                    targetTraits[i] = "Error"; break;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //
+    public GameObject currentTarget;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SpawnTarget();
+
+    }
+
+    void SpawnTarget()
+    {
+        GameObject targetModel = Resources.Load("Target") as GameObject;
+        currentTarget = Instantiate(targetModel, new Vector3(20, 0, 20), Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
